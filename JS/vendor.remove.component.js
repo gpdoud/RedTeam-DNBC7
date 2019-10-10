fd29b9fe4aa03de87304221130971070cd7af17c
Justin Dudley <justindudley99@gmail.com>
2019-10-10 16:32:12 -0400

    created vendor.remove.component.js

---
+++ JS/vendor.remove.component.js
@@ -0,0 +1,42 @@
+
+$().ready( () => {
+
+    $("#btnGet").click( () => { 
+        let id = $("#xid").val();
+        get(id);
+    });
+    $("#btnRemove").click( () => {
+        let vendor = {
+            id : $("#pid").val(),
+            code : $("#pcode").val(),
+            name : $("#pname").val(),
+            address : $("#paddress").val(),
+            city : $("#pcity").val(),
+            state : $("#pstate").val(),
+            zip : $("#pzip").val(),
+            phone : $("#pphone").val(),
+            email : $("#pemail").val() 
+        }
+        console.log("Vendor:", vendor);
+        VendorService.remove(vendor) 
+            .done((res) => {
+                console.log("Remove rc:", res);
+            });
+    }); 
+});
+
+const get = (id) => {
+    VendorService.get(id)
+        .done( (vendor) => {
+            $("#pid").val(vendor.id);
+            $("#pcode").val(vendor.code);
+            $("#pname").val(vendor.name);
+            $("#paddress").val(vendor.address);
+            $("#pcity").val(vendor.city);
+            $("#pstate").val(vendor.state);
+            $("#pzip").val(vendor.zip);
+            $("#pphone").val(vendor.phone);
+            $("#pemail").val(vendor.email);
+        });
+};
+
